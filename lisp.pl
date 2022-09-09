deb49d576cbf3dc4ab767d355ef2dcc9882da161
Cactus <sblaplace@gmail.com>
2022-09-09 15:30:45 +0200

    add tokenization

--- lisp.pl
+++ lisp.pl
@@ -1,6 +1,22 @@
 :- use_module(library(dcgs)).
 :- use_module(library(pio)).
+:- use_module(library(charsio)).
 
-tokenize(Tokens) --> 
+tokenize(["("|As]) --> ['('], tokenize(As).
+tokenize([")"|As]) --> [')'], tokenize(As).
+tokenize([A|As]) -->
+    spaces(_),
+    chars([X|Xs]),
+    {atom_chars(A, [X|Xs])},
+    spaces(_),
+    tokenize(As).
+tokenize([]) --> [].
 
-ws --> ([] | " " | "\n").
+chars([X|Xs]) --> char(X), !, chars(Xs).
+chars([]) --> [].
+
+spaces([X|Xs]) --> space(X), !, spaces(Xs).
+spaces([]) --> [].
+
+space(X) --> [X], {char_type(X, whitespace)}.
+char(X) --> [X], {\+ char_type(X, whitespace), X \= ')', X \= '(' }.
