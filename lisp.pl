bb65140173f282ea077a333e6cbb68d6183fc3b2
Cactus <sblaplace@gmail.com>
2022-09-10 18:27:08 +0200

    Update parsing, add number check

--- lisp.pl
+++ lisp.pl
@@ -2,11 +2,14 @@
 :- use_module(library(pio)).
 :- use_module(library(charsio)).
 :- use_module(library(si)).
+:- use_module(library(lists)).
+:- use_module(library(lambda)).
 
-parse([AST]) --> ['('], parse(AST), [')'].
-parse(AST) --> Ls, { atom_codes(Ls, Cs), number_codes(AST, Cs) }.
-parse(_) --> ... .
-
+parse([]) --> [].
+parse([ASTl]) --> ["("], parse(ASTl), [")"], parse(_).
+parse([AST]) --> ["("], parse(AST), [")"].
+parse([ASTh|ASTt]) --> [A], { atom_si(A), atom_chars(A, Cs), chars_are_nums(Cs), number_chars(ASTh, Cs) }, parse(ASTt).
+parse([ASTh|ASTt]) --> [ASTh], parse(ASTt).
 
 tokenize(["("|As]) --> spaces(_), ['('], spaces(_), tokenize(As).
 tokenize([")"|As]) --> spaces(_), [')'], spaces(_), tokenize(As).
@@ -26,3 +29,9 @@ spaces([]) --> [].
 
 space(X) --> [X], {char_type(X, whitespace)}.
 char(X) --> [X], {\+ char_type(X, whitespace), X \= ')', X \= '(' }.
+
+chars_are_nums(Cs) :- maplist(\C^(char_type(C, numeric)), Cs).
+
+eos([], []).
+
+standard_env({(+): (+), (-): (-)}).
